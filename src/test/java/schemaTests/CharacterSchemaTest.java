8a4febf629a791eb2ab299e19efbaa4d71fa9cd6
Martin Diaz <panaderodiaz98@gmail.com>
2024-11-21 19:52:56 -0300

    subiendo archivos adicionales

---
+++ src/test/java/schemaTests/CharacterSchemaTest.java
@@ -0,0 +1,31 @@
+package schemaTests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Link;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class CharacterSchemaTest {
+    private String baseUri = "https://rickandmortyapi.com/api/character";
+    @Test(groups = {"regresion"})
+    @Epic("Regresion")
+    @Feature("Schema Tests")
+    @Description("Validamos el cuerpo JSON de respuesta correspendientes a Characters segun documentacion")
+    @Link("https://rickandmortyapi.com/documentation/#character-schema")
+    public void characterSchemaTest(){
+//        Creamos request del tipo GET
+        given()
+                .contentType("application/json")
+                .when()
+                .baseUri(baseUri)
+                .get("/1")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/characterSchema.json"));
+    }
+}
---
+++ src/test/java/schemaTests/LocationSchemaTest.java
@@ -0,0 +1,31 @@
+package schemaTests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Link;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class LocationSchemaTest {
+    private String baseUri = "https://rickandmortyapi.com/api/location";
+    @Test(groups = {"regresion"})
+    @Epic("Regresion")
+    @Feature("Schema Tests")
+    @Description("Validamos el cuerpo JSON de respuesta correspendientes a Locations segun documentacion")
+    @Link("https://rickandmortyapi.com/documentation/#location-schema")
+    public void locationSchemaTest(){
+        given()
+                .contentType("application/json")
+                .when()
+                .baseUri(baseUri)
+                .when()
+                .get("/1")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/locationSchema.json"));
+    }
+}
